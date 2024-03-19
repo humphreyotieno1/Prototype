53c7e5c7bf3ddfa985184a7cec43db193225965f
emmanuel tindi <emmanueltindi66@gmail.com>
2024-03-19 23:07:22 +0300

    udated templates

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 
 from flask import Flask, render_template
 
-app = Flask('templates')
+app = Flask(__name__)
 
 @app.route('/landing')
 def landing():
-    return render_template('landing.html')
+    return render_template('landingpage.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='5000')
